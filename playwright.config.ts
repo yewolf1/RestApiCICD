19d14844dfae79238433e03d163c317cfee5c88b
yewolf1 <daniel.antunesdossantos@my-digital-school.org>
2025-01-30 11:27:33 +0100

    fourth commit

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "playwright/test";
 export default defineConfig({
   testDir: "./tests/e2e",
   timeout: 30000,
-  retries: 2,
+  retries: 1,
   webServer: {
     command: "npm start",
     port: 3000,
